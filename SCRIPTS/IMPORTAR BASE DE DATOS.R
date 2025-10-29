1988f8cd3919ad9b58103a8b9bf1e26aec4bf5a8
nataliaveronicapastrana <nataliaveronicapastrana@gmail.com>
2025-10-29 19:39:06 -0300

    SE SACO CODIGOS PARA ACTIVAR LIBRERIAS

--- SCRIPTS/IMPORTAR BASE DE DATOS.R
+++ SCRIPTS/IMPORTAR BASE DE DATOS.R
@@ -1,24 +1,4 @@
 # ----------------------------------------------------------------
-# CARGA DE LIBRERIAS
-# ----------------------------------------------------------------
-
-library(dplyr)
-library(ggplot2)
-library(lubridate)
-library(stringr)
-library(readxl)
-library(readr)
-library(writexl)
-library(readr)
-library(highcharter)
-library(tidyr)
-library(mapview)
-library(leaflet)
-library(geoAr)
-library(gt)
-library(htmltools)
-
-# ----------------------------------------------------------------
 # IMPORTAR DE BASE DE DATOS
 # ----------------------------------------------------------------
 
