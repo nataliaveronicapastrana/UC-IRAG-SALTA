11e6474c7e7a68cc7e5e5f016c0f23567d9db679
nataliaveronicapastrana <nataliaveronicapastrana@gmail.com>
2025-10-29 20:33:44 -0300

    ACTIVACION DE LIBRERIAS

--- SCRIPTS/ACTIVACION DE LIBRERIAS.R
+++ SCRIPTS/ACTIVACION DE LIBRERIAS.R
@@ -1,29 +1,4 @@
 #-----------------------------------------------------------------------------------
-#INSTALACION DE PAQUETES
-#-----------------------------------------------------------------------------------
-
-install.packages(c(
-  "dplyr",
-  "ggplot2",
-  "lubridate",
-  "stringr",
-  "readxl",
-  "readr",
-  "writexl",
-  "highcharter",
-  "tidyr",
-  "mapview",
-  "leaflet",
-  "geoAr",
-  "gt",
-  "htmltools"
-))
-
-install.packages("devtools")
-
-devtools::install_github("krassowski/complex-upset")
-
-#-----------------------------------------------------------------------------------
 #ACTIVACION DE LIBRERIAS
 #-----------------------------------------------------------------------------------
 
