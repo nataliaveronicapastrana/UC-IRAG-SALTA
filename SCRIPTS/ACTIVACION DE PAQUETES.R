55cfebbf4766266a145e26322b254e2b77d08e65
nataliaveronicapastrana <nataliaveronicapastrana@gmail.com>
2025-11-06 22:01:02 -0300

    ACTIVACION DE PAQUETES

---
+++ SCRIPTS/ACTIVACION DE PAQUETES.R
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------
+#ACTIVACION DE DE PAQUETES
+#-------------------------------------------------------------------------------
+
+# Activación de paquetes 
+
+library(dplyr)
+library(ggplot2)
+library(lubridate)
+library(stringr)
+library(readxl)
+library(readr)
+library(writexl)
+library(highcharter)
+library(tidyr)
+library(tidyverse)
+library(mapview)
+library(leaflet)
+library(geoAr)
+library(gt)
+library(htmltools)
+library(devtools)
+library(ComplexUpset)
