1d259966f7cf24f8c3a4d3794ba09c0e9ecd3960
mercedespaz <pazmercedes.mp@gmail.com>
2025-11-20 17:07:15 -0300

    Modificaciones importación base agrupada.

--- SCRIPTS/IMPORTAR BASE DE DATOS.R
+++ SCRIPTS/IMPORTAR BASE DE DATOS.R
@@ -2,25 +2,30 @@
 # IMPORTAR DE BASE DE DATOS NOMINAL Y AGRUPADA
 # ------------------------------------------------------------------------------
 
+#BASE NOMINAL
 DATA_UC_IRAG <- read_delim(here("TEMPLATES", "UC_IRAG_HSM.csv"), delim = ";",
   locale = locale(encoding = "latin1"),
   trim_ws = TRUE)
 
-View(DATA_UC_IRAG)
+#View(DATA_UC_IRAG)
 
 
+#BASE AGRUPADA
+DATA_UC_IRAG_AGRUPADA <- read_excel( here("TEMPLATES", "UC_IRAG_AGRUPADA_HSM.xlsx"))
 
-DATA_UC_IRAG_AGRUPADA <- read_excel( here("TEMPLATES", "UC_IRAG_AGRUPADA_HSM.xlsx"),
-  col_types = c("text", "numeric", "numeric",
-    "text", "text", "text", "text",
-    "numeric", "numeric", "numeric", "numeric",
-    "numeric", "numeric", "numeric",
-    "numeric", "numeric", "numeric",
-    "numeric", "numeric", "numeric",
-    "numeric", "numeric", "numeric",
-    "numeric", "numeric", "numeric",
-    "numeric", "numeric"
-  ))
+DATA_UC_IRAG_AGRUPADA <-DATA_UC_IRAG_AGRUPADA[-1,]
 
-View(DATA_UC_IRAG_AGRUPADA)
+  # col_types = c("text", "numeric", "numeric",
+  #   "text", "text", "text", "text",
+  #   "numeric", "numeric", "numeric", "numeric",
+  #   "numeric", "numeric", "numeric",
+  #   "numeric", "numeric", "numeric",
+  #   "numeric", "numeric", "numeric",
+  #   "numeric", "numeric", "numeric",
+  #   "numeric", "numeric", "numeric",
+  #   "numeric", "numeric"
+  # ))
+
+
+#View(DATA_UC_IRAG_AGRUPADA)
 
